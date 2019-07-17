18e98c5e66eb4cef9e2e3970fdf51f316b0f9c01
suchenxiaoyu <suchenxiaoyu@gmail.com>
2019-07-17 16:47:29 +0800

    alias

--- config/index.js
+++ config/index.js
@@ -45,8 +45,13 @@ const config = {
   },
   defineConstants: {},
   alias: {
+    '@/actions': path.resolve(__dirname, '..', 'src/actions'),
+    '@/assets': path.resolve(__dirname, '..', 'src/assets'),
     '@/components': path.resolve(__dirname, '..', 'src/components'),
-    '@/actions': path.resolve(__dirname, '..', 'src/actions')
+    '@/constants': path.resolve(__dirname, '..', 'src/constants'),
+    '@/reducers': path.resolve(__dirname, '..', 'src/reducers'),
+    '@/styles': path.resolve(__dirname, '..', 'src/styles'),
+    '@/utils': path.resolve(__dirname, '..', 'src/utils')
   },
   copy: {
     patterns: [],
