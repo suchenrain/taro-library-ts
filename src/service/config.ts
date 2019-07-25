528616accec157bfca19211ba4978c13708a80e0
suchenxiaoyu <suchenxiaoyu@gmail.com>
2019-07-25 09:26:50 +0800

    prod url

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,6 +1,6 @@
 const devURL: string = 'http://localhost:3000';
 
-const prodURL: string = '';
+const prodURL: string = 'https://gulp-mock-now.suchenxiaoyu.now.sh';
 
 const BASE_URL = process.env.NODE_ENV === 'development' ? devURL : prodURL;
 
